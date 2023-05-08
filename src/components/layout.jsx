e1778e23588a5651f67d4547e50a4ca6b3759935
gohguancheng <gohguancheng01@gmail.com>
2023-05-08 12:18:54 +0800

    try next css modules

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,27 +1,30 @@
 import Link from "next/link";
 import { useRouter } from 'next/router'
+import style from './styles.module.css'
 
 export default function Layout({ children }) {
   const router = useRouter()
 
   const navLinks = [
     { id: "home", label: "Home", href: "/" },
-    { id: "services", label: "Services", href: "/services" },
+    {
+      id: "services", label: "Services", href: "/services"
+    },
   ];
 
   return (
-    <div className="w-screen min-h-screen flex flex-col">
-      <div className="navbar px-[1.5rem] bg-white shadow-black shadow-md">
-        <nav className="w-full max-w-[720px] lg:max-w-[1300px] mx-auto flex items-center justify-between">
-          <Link href="/" className="text-[#3C3C3B] capitalize text-5xl font-bold">
+    <div className={style.container}>
+      <div className={style.topbar}>
+        <nav className={style.linksContainer}>
+          <Link href="/" className={style.logoLink}>
             Life
             <span className="text-[#E73331] uppercase">sg</span>
           </Link>
 
           <div>
-            <ul className="flex gap-[1rem]">
+            <ul className={style.navLinksContainer}>
               {navLinks.map(({ id, label, href }) => (<li key={id}>
-                <Link href={href} className={`block py-[20px] text-black border-b-[4px] ${router.route == href ? 'border-blue-600 font-semibold' : 'border-white'}`} >{label}</Link>
+                <Link href={href} className={`${style.navLink} ${router.route == href ? style.currentLink : ''}`} >{label}</Link>
               </li>))}
             </ul>
           </div>
